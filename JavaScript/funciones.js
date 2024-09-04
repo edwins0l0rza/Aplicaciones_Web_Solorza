17d6cc85527ac392f35d9be6b35e4f5d7c852e58
EDWIN ALFREDO SOLORZA OCHOA <169114867+edwins0l0rza@users.noreply.github.com>
2024-09-03 20:51:04 -0600

    Update funciones.js

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -1,9 +1,11 @@
 /* Pregunta: ¿Cuál es la función de 'document.getElementById' en JavaScript? */
+/*La función document.getElementById en JavaScript se utiliza para seleccionar y acceder a un elemento del DOM (Document Object Model)*/
         function checkAnswer() {
             /* Pregunta: ¿Qué hace 'prompt' y cómo se puede usar en lugar de 'alert'? */
             let answer = prompt("Enter your answer:");
 
             /* Pregunta: ¿Cuál es el propósito de la instrucción 'if' en este fragmento de código? */
+                /*Es una condicion que nos indica que si la respuesta es  igual a 'paris' lance una alerta de que es correcta*/
             if (answer.toLowerCase() === 'paris') {
                 alert("Correct!");
             } else {
